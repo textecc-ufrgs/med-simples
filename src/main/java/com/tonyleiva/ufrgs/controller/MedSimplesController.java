02e9520b7ae64fd36228637787ece5f1414b561b
Tony <tonyleiva@hotmail.com>
2020-05-01 17:02:28 -0300

    Add Cross Origin to /simplify request

--- src/main/java/com/tonyleiva/ufrgs/controller/MedSimplesController.java
+++ src/main/java/com/tonyleiva/ufrgs/controller/MedSimplesController.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -33,6 +34,7 @@ public class MedSimplesController {
 	@Autowired
 	private ObjectMapper objectMapper;
 
+	@CrossOrigin
 	@PostMapping(value = "/simplify", consumes = MediaType.TEXT_PLAIN_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> simplify(@RequestHeader(value = "Content-Type") String contentType,
 			@RequestBody String textBody) {
